7b6da1eddd58556aa6b9d79d9493298fa1b21dd4
Zinadin Zidan <zidan44@pixelexperience.org>
2021-03-21 00:20:06 +0600

    apollo-sepolicy: Adopt it for apollo
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I0132a906cedc9018080d4c9c256989d099801d12

---
+++ apollo-sepolicy.mk
@@ -0,0 +1,36 @@
+#BOARD_PLAT_PUBLIC_SEPOLICY_DIRS += \
+#    device/xiaomi/apollo-sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/xiaomi/apollo-sepolicy/private
+
+# NXP Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/apollo-sepolicy/vendor/nxp
+
+# Lineage Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/apollo-sepolicy/vendor/lineage
+
+# Qcom Sepolicy
+BOARD_SEPOLICY_DIRS += \
+	device/xiaomi/apollo-sepolicy/vendor/qcom
+
+# Xiaomi Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/audio \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/battery \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/camera \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/fingerprint \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/fod \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/last_kmsg \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/light \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/motor \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/mlipay \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/parts \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/power \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/radio \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/thermald \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/usb \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/wireless \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/wlan
