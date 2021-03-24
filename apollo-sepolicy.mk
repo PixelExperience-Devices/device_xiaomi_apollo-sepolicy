524bf88e379c194b1d056d44ecd1144837aa743d
Zinadin Zidan <zidan44@pixelexperience.org>
2021-03-24 11:31:05 +0600

    apollo-sepolicy: Label citsensor and grant required permissions
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Iaabcb289a56f6d1a91526af5ed2c85d52a1e88bb

--- apollo-sepolicy.mk
+++ apollo-sepolicy.mk
@@ -21,6 +21,7 @@ BOARD_SEPOLICY_DIRS += \
     device/xiaomi/apollo-sepolicy/vendor/xiaomi/audio \
     device/xiaomi/apollo-sepolicy/vendor/xiaomi/battery \
     device/xiaomi/apollo-sepolicy/vendor/xiaomi/camera \
+    device/xiaomi/apollo-sepolicy/vendor/xiaomi/citsensor \
     device/xiaomi/apollo-sepolicy/vendor/xiaomi/fingerprint \
     device/xiaomi/apollo-sepolicy/vendor/xiaomi/fod \
     device/xiaomi/apollo-sepolicy/vendor/xiaomi/last_kmsg \
